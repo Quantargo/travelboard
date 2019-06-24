6dfd9e5776adfc8e0612d1c4194da33794159524
Michael Mifek <mifek@me.com>
2019-06-24 15:39:56 +0200

    draft UI

--- R/mod_statistics.R
+++ R/mod_statistics.R
@@ -1,3 +1,8 @@
+# Load data ---------------------------------------------------------------
+bli <- readRDS(file = "~/workshop/data/oecd/bli.rds")
+pppgdp <- readRDS(file = "~/workshop/data/oecd/pppgdp.rds")
+eo <- readRDS(file = "~/workshop/data/oecd/eo.rds")
+
 # Module UI
   
 #' @title   mod_statistics_ui and mod_statistics_server
@@ -16,20 +21,20 @@
 mod_statistics_ui <- function(id, dest){
   ns <- NS(id)
   
-  # This is just an example UI to be modified
-  # Please change for your purpose
-  # Do not forget to put ns() around all input ids!!!
-  tagList(
-    tags$h1(paste(dest, "Statistics", sep = "-")),
-    fluidRow(
-      box(title = dest, plotOutput(ns("plot1"), height = 250)),
-      
-      box(
-        title = "Controls",
-        sliderInput(ns("slider"), "Number of observations:", 1, 100, 50)
-      )
-    )
-  )
+  selectInput("countries",
+              label = "Choose your countries",
+              multiple = TRUE,
+              choices = NULL)
+  radioButtons("gender", "Choose sex", c("all", "female", "male"), selected = "all")
+  
+  plotOutput("plot")
+  
+  selectInput("indicator",
+              label = "Choose your indicator",
+              multiple = TRUE,
+              choices = NULL)
+  
+  
 }
     
 # Module Server
@@ -37,17 +42,13 @@ mod_statistics_ui <- function(id, dest){
 #' @rdname mod_statistics
 #' @export
 #' @keywords internal
-    
 mod_statistics_server <- function(input, output, session, dest){
   ns <- session$ns
   
-  # This is just an example Server to be modified
-  # Please change for your purpose
-  histdata <- rnorm(500)
-  output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data, main = dest())
-  })
+  all_countries <- sort(unique(bli$LOCATION))
+  #updateSelectInput(session, inputId = "countries", choices = all_countries, selected = dest)
+
+    
 }
     
 ## To be copied in the UI
