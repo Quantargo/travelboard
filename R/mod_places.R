c8e3de2901c5c3bd0c2b53f03e9bd4c590cf26e4
apeterseil <andreas.peterseil@rcb.at>
2019-06-25 10:34:03 +0200

    Replaced google_map by leaflet

--- R/mod_places.R
+++ R/mod_places.R
@@ -12,57 +12,48 @@
 #'
 #' @keywords internal
 #' @export 
-#' @importFrom shiny NS tagList
+#' @importFrom shiny NS tagList 
+#' @import dplyr leaflet leaflet.extras readr 
+
 mod_places_ui <- function(id, dest){
   ns <- NS(id)
-  # data1 <- reactive({
-  #   read_rds(paste0("~/workshop/data/google_places/",tolower(dest),".rds"))[["Restaurants"]]
-  # })
-  # 
-  # This is just an example UI to be modified
-  # Please change for your purpose
-  # Do not forget to put ns() around all input ids!!!
-  tabBox(width = 12,
-         tabPanel("Restaurants",
-                  fluidRow(column(width = 8,
-                                  google_mapOutput(outputId = ns("map"))), 
-                           column(width = 4,
-                                  sliderInput(inputId = ns("pricelevel"),
-                                              label = "Price Level",
-                                              #min = min(data1()$price_level,na.rm = TRUE), max = max(data1()$price_level,na.rm = TRUE),
-                                              min = 1, max = 3,
-                                              value = c(1,3),
-                                              step = 1),
-                                  sliderInput(inputId = ns("rating"),
-                                              label = "Rating",
-                                              #min = min(data1()$rating,na.rm = TRUE), max = max(data1()$rating,na.rm = TRUE),
-                                              min = 1, max = 5,
-                                              value = c(4,5),
-                                              step = 0.1),
-                                  sliderInput(inputId = ns("numberofratings"),
-                                              label = "Number of Ratings",
-                                              #min = min(data1()$user_ratings_total,na.rm = TRUE), max = max(data1()$user_ratings_total,na.rm = TRUE),
-                                              min = 1, max = 5000,
-                                              value = c(1,5000)))),
-                  fluidRow(
-                    valueBox(10 * 2, "New Orders", icon = icon("credit-card"),width = 4)#,
-                    #valueBoxOutput(outputId = ns("ratingbox"))
-                  )
-         ),
-         tabPanel("Bars",
-                  fluidRow(column(width = 6,"plot"), column(width = 6,"slideinputs"))
-         ),
-         tabPanel("Museums",
-                  fluidRow(column(width = 6,"plot"), column(width = 6,"slideinputs"))
-         ),
-         tabPanel("Cinemas",
-                  fluidRow(column(width = 6,"plot"), column(width = 6,"slideinputs"))
-         ),
-         tabPanel("Discos",
-                  fluidRow(column(width = 6,"plot"), column(width = 6,"slideinputs"))
-         )
-  )
   
+  fluidPage(
+    fluidRow(
+      checkboxGroupInput(inputId = ns("selected_type"),
+                         label = "Select Places",
+                         choices = c("Restaurants", "Bars", "Cinemas","Disco","Museums"),
+                         selected = "Restaurants")
+    ),
+    fluidRow(
+      fluidRow(column(width = 8,
+                      leafletOutput(outputId = ns("map"))
+      ), 
+      column(width = 4,
+             sliderInput(inputId = ns("pricelevel"),
+                         label = "Price Level",
+                         min = 1, max = 5,
+                         value = c(1,3),
+                         step = 1),
+             sliderInput(inputId = ns("rating"),
+                         label = "Rating",
+                         min = 1, max = 5,
+                         value = c(4,5),
+                         step = 0.1),
+             sliderInput(inputId = ns("numberofratings"),
+                         label = "Number of Ratings",
+                         min = 1, max = 10000,
+                         value = c(1,5000)),
+             uiOutput(outputId = "slideroutput"))),
+      fluidRow(
+        br(),
+        valueBoxOutput(outputId = ns("averagerating"),width = 2),
+        valueBoxOutput(outputId = ns("numberofobjects"),width = 2),
+        valueBoxOutput(outputId = ns("averageprice"),width = 2),
+        valueBoxOutput(outputId = ns("numberofreviews"),width = 2)
+      )
+    )
+  )
 }
 
 # Module Server
@@ -76,23 +67,53 @@ mod_places_server <- function(input, output, session, dest){
   ns <- session$ns
   key <- "AIzaSyBCGvNSks4_NvBcAwdRLw9hXM0J0RkQhQg"
   # 
-  # data <- reactive({
-  #   read_rds(paste0("~/workshop/data/google_places/",tolower(dest()),".rds"))[["Restaurants"]]
-  # })
+  data <- reactive({
+    read_rds(paste0("~/workshop/data/google_places/",tolower(dest()),".rds"))[["Restaurants"]]
+  })
   
   data <- reactive({
-    get_places_data(dest,"Restaurants",
+    req(input$selected_type)
+    get_places_data(dest,input$selected_type[1],
                     c("price_level","rating","user_ratings_total"),
                     c(input$pricelevel[1],input$rating[1],input$numberofratings[1]),c(input$pricelevel[2],input$rating[2],input$numberofratings[2]) )
   })
   
-  output$map <- renderGoogle_map(google_map(data = data(), location = NULL, key = key) %>%
-                                   add_markers(lat = "lat", lon = "lng", info_window = "info") %>%
-                                   add_heatmap(lat = "lat", lon = "lng", weight = "user_ratings_total", option_radius = 0.15, legend = TRUE))
+  output$map <- renderLeaflet({
+    leaflet(data()) %>%
+      addTiles() %>% 
+      addMarkers(lng=data()$lng, 
+                 lat=data()$lat, popup=data()$name, 
+                 clusterOptions = markerClusterOptions())
+  })
+  output$averagerating <- renderValueBox({
+    valueBox(
+      round(mean(data()$rating),1), "Average Rating", icon = icon("smile"),
+      color = "yellow"
+    )
+  })
   
-  # output$ratingbox <- renderValueBox({
-  #   valueBox(value = "80",subtitle = "Average Rating",width = 4)
-  # })
+  output$numberofobjects <- renderValueBox({
+    valueBox(
+      nrow(data()), "Number of places", icon = icon("map-marked"),
+      color = "purple"
+    )
+  })
+  
+  output$averageprice <- renderValueBox({
+    valueBox(
+      round(mean(data()$price_level,na.rm = TRUE),1), "Average price level", 
+      icon = icon("euro-sign"),
+      color = "blue"
+    )
+  })
+  
+  output$numberofreviews <- renderValueBox({
+    valueBox(
+      round(mean(data()$user_ratings_total,na.rm = TRUE),0), "Average number of reviews", icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "fuchsia"
+    )
+  })
+  
   
   
 }
